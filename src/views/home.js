f4e35e02958cedc2775900d0cc808357b68b53b1
JenifferGenoves <jenifer.genoves03@gmail.com>
2020-10-01 09:09:14 -0300

    Arreglo de errores

--- src/views/home.js
+++ src/views/home.js
@@ -5,12 +5,9 @@ import ImgPage from '../components/ImgPage'
 import Footer from '../components/Footer'
 import DonatePrice from '../components/Donate-price'
 import OtherDonate from '../components/OtherDonate'
-<<<<<<< HEAD
 import CollabSection from '../components/CollabSection.jsx';
-=======
 import Carrousel from '../components/Carrousel';
 import Insta from '../components/Instagram';
->>>>>>> a0fcc4ad6ea2010ac6415976f9b3c0f2b62f54bc
 
 function Home() {
   return (
@@ -27,13 +24,10 @@ function Home() {
           <Carrousel />
           <OtherDonate />
         </div>
-<<<<<<< HEAD
         <div>
           <CollabSection />
         </div>
-=======
         <Insta />
->>>>>>> a0fcc4ad6ea2010ac6415976f9b3c0f2b62f54bc
       </body>
       <div className="containerFooter">
         <Footer />
