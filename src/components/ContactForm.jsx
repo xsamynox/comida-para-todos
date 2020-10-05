54883debe12fcc8af6512cf0b6c3896796a1ba1b
Samantha <xsamynox@gmail.com>
2020-10-05 12:55:13 -0300

    05-10-2020 changes contact

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import '../styles/contactForm.css';
+
+const ContactForm = () => {
+    return (
+        <Fragment>
+            <div className="contactForm">
+
+            </div>
+        </Fragment>
+    )
+}
+export default ContactForm;
