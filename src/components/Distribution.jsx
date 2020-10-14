dca1ac0d68528bc20d9d99646c35f19a68c3b911
Samantha <xsamynox@gmail.com>
2020-10-14 15:51:09 -0300

    popover

--- src/components/Distribution.jsx
+++ src/components/Distribution.jsx
@@ -13,19 +13,33 @@ const Distribution = () => {
           <p><strong>El 65% de los recursos de la Fundación</strong> se destinan a los Restaurantes, los que se encargan de realizar las comidas, apadrinar  comunidades vunerables entregándoles capacitaciones, además de compartir buenas prácticas sobre alimentación saludable.</p>
           <div className='list-distribution'>
             <div className='Column1'>
-            <div className="circulo1"><p className='titlecrc'>Restaurantes</p></div>
+              <div className="circulo1"><p className='titlecrc'>Restaurantes</p></div>
               <div className="circulo2"><p className='titlecrc'>Operaciones</p></div>
               <div className="circulo3"><p className='titlecrc'>Comunicaciones</p></div>
-              </div>
-              <div className='Column2'>
+            </div>
+            <div className='Column2'>
               <div className="circulo4"><p className='titlecrc'>Administración</p></div>
-             <div className="circulo5"><p className='titlecrc'>Transporte</p></div>  
+              <div className="circulo5"><p className='titlecrc'>Transporte</p></div>
             </div>
           </div>
-        </div>  
-        <div>
-          <img className='chartimg' src={charts} alt='grafica' />
         </div>
+        {/* <div>
+          <img className='chartimg' src={charts} alt='grafica' />
+        </div> */}
+
+        <div className="popover__wrapper">
+          <a>
+            <img className='chartimg' src={charts} alt='grafica' />
+          </a>
+          <div className="popover__content" >
+            <div className="circulo1"><p className='titlecrc'>65%</p></div>
+            <div className="circulo2"><p className='titlecrc'>18%</p></div>
+            <div className="circulo3"><p className='titlecrc'>9%</p></div>
+            <div className="circulo4"><p className='titlecrc'>2%</p></div>
+            <div className="circulo5"><p className='titlecrc'>6%</p></div>
+          </div>
+        </div>
+
       </div>
       <div className='ctn-documents'>
         <h2>Descarga el documento completo</h2>
@@ -35,3 +49,4 @@ const Distribution = () => {
   );
 }
 export default Distribution;
+
--- src/components/Impacto.jsx
+++ src/components/Impacto.jsx
@@ -8,19 +8,19 @@ const Impacto = () => {
       <h2 className='impact'>Impacto</h2>
       <div className='ctn-impact' style={{ color: "white" }}>
         <div>
-          <CountUp className='number-impact' start={-100} end={46} duration={6} suffix='M' />
+          <CountUp className='number-impact' start={-100} end={46} duration={10} delay={3} suffix='M' />
           <p className='letter-impact'>Donaciones</p>
         </div>
         <div>
-          <CountUp className='number-impact' end={160} duration={6} start={-100} suffix='K' />
+          <CountUp className='number-impact' end={160} duration={10} start={-100} delay={3} suffix='K' />
           <p className='letter-impact'>Almuerzos<br /> Recaudados</p>
         </div>
         <div>
-          <CountUp className='number-impact' end={72} duration={6} start={-100} />
+          <CountUp className='number-impact' end={72} duration={10} start={-100} delay={3} />
           <p className='letter-impact'>Puestos<br /> de Trabajo</p>
         </div>
         <div>
-          <CountUp className='number-impact' end={12} duration={6} start={-100} />
+          <CountUp className='number-impact' end={12} duration={10} start={-100} delay={3} />
           <p className='letter-impact'>Comunas</p>
         </div>
       </div>
