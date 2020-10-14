748bfd2c6c2076c108a19df75c88fefeb799a7c8
Ane Acevedo <ane.acevedo@gmail.com>
2020-10-14 12:24:13 -0300

    Solución sección de gráficos en transparencia

--- src/components/Distribution.jsx
+++ src/components/Distribution.jsx
@@ -10,20 +10,22 @@ const Distribution = () => {
         <div className='ctn-distribution'>
           <h4>Distribución</h4>
           <h2>Costos de la donación</h2>
-          <p><strong>El 65% de los recursos de la Fundación</strong> se destinan a los Restaurantes, los que se encargan de realizar las comidas, apadrinar  comunidades vunerables entregándoles capacitaciones, además de compartir buenas prácticas sobre alimentación saludable.
-          <br/>
-          Los valores están expresados en %</p>
-          </div>
-          <div className='contentchart'>
-          <img className='chartimg' src={charts} alt='grafica' />
+          <p><strong>El 65% de los recursos de la Fundación</strong> se destinan a los Restaurantes, los que se encargan de realizar las comidas, apadrinar  comunidades vunerables entregándoles capacitaciones, además de compartir buenas prácticas sobre alimentación saludable.</p>
           <div className='list-distribution'>
+            <div className='Column1'>
             <div className="circulo1"><p className='titlecrc'>Restaurantes</p></div>
               <div className="circulo2"><p className='titlecrc'>Operaciones</p></div>
               <div className="circulo3"><p className='titlecrc'>Comunicaciones</p></div>
+              </div>
+              <div className='Column2'>
               <div className="circulo4"><p className='titlecrc'>Administración</p></div>
              <div className="circulo5"><p className='titlecrc'>Transporte</p></div>  
+            </div>
           </div>
         </div>  
+        <div>
+          <img className='chartimg' src={charts} alt='grafica' />
+        </div>
       </div>
       <div className='ctn-documents'>
         <h2>Descarga el documento completo</h2>
