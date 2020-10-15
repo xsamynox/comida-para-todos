03a872d34c0f186c7f09a140feeefbec8c801575
Ane Acevedo <ane.acevedo@gmail.com>
2020-10-15 09:12:55 -0300

    Arreglo opacity

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -5,7 +5,7 @@ const SearchBar = () => {
     return (
         <Fragment>
 <div className="wrap">
-  <form className="form-1" action="" autocomplete="on">
+  <form className="form-1" action="" autoComplete="on">
   <input className="input-1" id="search" name="search" type="text" placeholder="Buscar"/><input className="input-1" id="search_submit" value="Rechercher" type="submit"/>
   </form>
 </div>
