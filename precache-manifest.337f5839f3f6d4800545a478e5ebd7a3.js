d1db7a8b47e10e42b2a2dd21a78842cc79d568aa
Samantha <xsamynox@gmail.com>
2020-10-15 08:32:18 -0300

    Updates

---
+++ precache-manifest.337f5839f3f6d4800545a478e5ebd7a3.js
@@ -0,0 +1,478 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34934ece9bf5f992322759bcfa88492b",
+    "url": "/comida-para-todos/index.html"
+  },
+  {
+    "revision": "b8848be04489aa605ff4",
+    "url": "/comida-para-todos/static/css/main.00ce66b0.chunk.css"
+  },
+  {
+    "revision": "8fb10e6522a12ff1ff1d",
+    "url": "/comida-para-todos/static/js/2.e6b9d90a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/comida-para-todos/static/js/2.e6b9d90a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8848be04489aa605ff4",
+    "url": "/comida-para-todos/static/js/main.2ffee23c.chunk.js"
+  },
+  {
+    "revision": "eb5763ed1c8cb0d36360",
+    "url": "/comida-para-todos/static/js/runtime-main.25453f12.js"
+  },
+  {
+    "revision": "280cffd5d352ca9950ae763c49d74bbf",
+    "url": "/comida-para-todos/static/media/18-sept.280cffd5.mp4"
+  },
+  {
+    "revision": "891bb3eb61ddbcd6ba670e46a6f4ebdf",
+    "url": "/comida-para-todos/static/media/1copia.891bb3eb.jpg"
+  },
+  {
+    "revision": "15e0487a2a62f143aa38476f46a7e014",
+    "url": "/comida-para-todos/static/media/Alejandra.15e0487a.jpg"
+  },
+  {
+    "revision": "12188bf283bb106b79adbb2dbf9dfcfb",
+    "url": "/comida-para-todos/static/media/Alimento.12188bf2.png"
+  },
+  {
+    "revision": "9046cdac9340eab5df2fe2e53ac073c6",
+    "url": "/comida-para-todos/static/media/Alimentos.9046cdac.png"
+  },
+  {
+    "revision": "53fe9bc808e28f8e3f8e1d9d68ef8dfe",
+    "url": "/comida-para-todos/static/media/BebasNeue-Regular.53fe9bc8.otf"
+  },
+  {
+    "revision": "86db24451a760ec66d4b8f5e8f7d7d2a",
+    "url": "/comida-para-todos/static/media/Camila.86db2445.png"
+  },
+  {
+    "revision": "f1272cbf9b2ffcdf4e69deb28c541ab9",
+    "url": "/comida-para-todos/static/media/Campaña18.f1272cbf.jpg"
+  },
+  {
+    "revision": "0d6cfcbb083eb04dfc19c8cbd563c4d5",
+    "url": "/comida-para-todos/static/media/Carlos.0d6cfcbb.jpg"
+  },
+  {
+    "revision": "939dbcc6bef691278f3244e2538bc6f4",
+    "url": "/comida-para-todos/static/media/CocinarEsAmar.939dbcc6.mp4"
+  },
+  {
+    "revision": "581173c537ec162cdff0a2ab502c9755",
+    "url": "/comida-para-todos/static/media/Colaboradores.581173c5.jpg"
+  },
+  {
+    "revision": "c50ee8c48686725dc1e6c261042475cd",
+    "url": "/comida-para-todos/static/media/Collaborators.c50ee8c4.png"
+  },
+  {
+    "revision": "dceb1af16375031868625b0a8bdb3c57",
+    "url": "/comida-para-todos/static/media/Comunidad.dceb1af1.png"
+  },
+  {
+    "revision": "21006f793546e67e4eff39f93a463fef",
+    "url": "/comida-para-todos/static/media/DiaMadre.21006f79.png"
+  },
+  {
+    "revision": "eb8317217ccf22158cd7bff37d836f08",
+    "url": "/comida-para-todos/static/media/DiaPadre.eb831721.png"
+  },
+  {
+    "revision": "ee7f5180ee39792018534246e726de8a",
+    "url": "/comida-para-todos/static/media/ENRIQUE.ee7f5180.png"
+  },
+  {
+    "revision": "ecb8b331b663cc0aa8393c57279d37de",
+    "url": "/comida-para-todos/static/media/ENZO.ecb8b331.png"
+  },
+  {
+    "revision": "5489ce2ce7791ddf1ef3e83eff88441c",
+    "url": "/comida-para-todos/static/media/Educacion.5489ce2c.png"
+  },
+  {
+    "revision": "dd9af9a839cb56a3e49171d147906b10",
+    "url": "/comida-para-todos/static/media/Eleanore.dd9af9a8.jpg"
+  },
+  {
+    "revision": "80b7c000a3cbfbbd863491459a2ee9f9",
+    "url": "/comida-para-todos/static/media/Empleo.80b7c000.png"
+  },
+  {
+    "revision": "66a16035f168e9abfb32097cdf6b6f97",
+    "url": "/comida-para-todos/static/media/GraciasxDonar.66a16035.png"
+  },
+  {
+    "revision": "badcae1f9c04a70f77ee47d2f1899168",
+    "url": "/comida-para-todos/static/media/GraciasxDonar.badcae1f.png"
+  },
+  {
+    "revision": "537c57dd0e1715c8cad9e17a6ac3758f",
+    "url": "/comida-para-todos/static/media/Habilidad.537c57dd.png"
+  },
+  {
+    "revision": "3e4fd60e6a405d541d0790ae16b7a38e",
+    "url": "/comida-para-todos/static/media/HuertoCom.3e4fd60e.png"
+  },
+  {
+    "revision": "666e99fc07a0a22a8874eb51bb8dfc1c",
+    "url": "/comida-para-todos/static/media/HuertoUrbano.666e99fc.png"
+  },
+  {
+    "revision": "71c69851f1ec600175d5886ec082e0b0",
+    "url": "/comida-para-todos/static/media/Huertos-Comunitarios.71c69851.jpg"
+  },
+  {
+    "revision": "379a6d779d996b432f1e216265a94061",
+    "url": "/comida-para-todos/static/media/LOUISA.379a6d77.png"
+  },
+  {
+    "revision": "c64e4f482667b24cd217def1c2db0618",
+    "url": "/comida-para-todos/static/media/MARIAJOSE.c64e4f48.png"
+  },
+  {
+    "revision": "f7768abd6e5ae1d4bd30c32b7f0ba46a",
+    "url": "/comida-para-todos/static/media/MEJOR-PAIS.f7768abd.mp4"
+  },
+  {
+    "revision": "ecc058c4cc7678a9738369ae0bfacd26",
+    "url": "/comida-para-todos/static/media/PlatoVacio.ecc058c4.png"
+  },
+  {
+    "revision": "21dfcfde0ebddcdb756f3a96fa94e4ce",
+    "url": "/comida-para-todos/static/media/Porcentaje12.21dfcfde.png"
+  },
+  {
+    "revision": "7ef3ddf315233af80cf594268300a32e",
+    "url": "/comida-para-todos/static/media/Porcentaje15.7ef3ddf3.png"
+  },
+  {
+    "revision": "e4e23d70913915d0b384abcfdef8e69c",
+    "url": "/comida-para-todos/static/media/Porcentaje27.e4e23d70.png"
+  },
+  {
+    "revision": "65c9d85b4c12fc20e02519f442fed0f4",
+    "url": "/comida-para-todos/static/media/PortQuienes-Somos.65c9d85b.jpg"
+  },
+  {
+    "revision": "d4e02bf5a140c84053bbe730710b091e",
+    "url": "/comida-para-todos/static/media/Portada-dona-aqui.d4e02bf5.jpg"
+  },
+  {
+    "revision": "15381d592271f411cefcbf18ef5b68ae",
+    "url": "/comida-para-todos/static/media/Portada.15381d59.png"
+  },
+  {
+    "revision": "1deeddc6faf96a3034f8ad4b4e878f2e",
+    "url": "/comida-para-todos/static/media/Portada.1deeddc6.png"
+  },
+  {
+    "revision": "408b5f1042de1b99763b2f0bb739acae",
+    "url": "/comida-para-todos/static/media/Portada.408b5f10.jpg"
+  },
+  {
+    "revision": "c0139e3e93e7ef58d8e8ef0e92662f8b",
+    "url": "/comida-para-todos/static/media/Portada.c0139e3e.png"
+  },
+  {
+    "revision": "d2cadba46f800c0e99f58a24ac847033",
+    "url": "/comida-para-todos/static/media/Portada.d2cadba4.jpg"
+  },
+  {
+    "revision": "7d807290276651f9aa1df66516a8deca",
+    "url": "/comida-para-todos/static/media/Portada2.7d807290.png"
+  },
+  {
+    "revision": "488f9c18921d4fd37423ae5e503ab22c",
+    "url": "/comida-para-todos/static/media/Portada_Campaña.488f9c18.jpg"
+  },
+  {
+    "revision": "33133680d5f2c5e3886e6cb3c303e39d",
+    "url": "/comida-para-todos/static/media/Portada_Equipo.33133680.jpg"
+  },
+  {
+    "revision": "9b567f9303a3686f2117a400ffd322c3",
+    "url": "/comida-para-todos/static/media/Portada_ProyEscuela.9b567f93.jpg"
+  },
+  {
+    "revision": "342cbed629b424548480b612c1858dca",
+    "url": "/comida-para-todos/static/media/ProyectoEscuela.342cbed6.png"
+  },
+  {
+    "revision": "aa66be9029958d51b3a5c0fbdcd3894f",
+    "url": "/comida-para-todos/static/media/SEBASTIAN.aa66be90.png"
+  },
+  {
+    "revision": "3b223029fbdc7ad5c22380a0379344fd",
+    "url": "/comida-para-todos/static/media/SemanaSanta.3b223029.png"
+  },
+  {
+    "revision": "e3767135307c90a06f7fab7ce2b09e37",
+    "url": "/comida-para-todos/static/media/T.Alejandra.e3767135.png"
+  },
+  {
+    "revision": "727a5785ea481d15f4038e2a1aa6155d",
+    "url": "/comida-para-todos/static/media/Testimonio.727a5785.png"
+  },
+  {
+    "revision": "a3786d6dd7ab0a237430fe552d3b4fd5",
+    "url": "/comida-para-todos/static/media/Testimonio.a3786d6d.png"
+  },
+  {
+    "revision": "15381d592271f411cefcbf18ef5b68ae",
+    "url": "/comida-para-todos/static/media/Voluntariado.15381d59.png"
+  },
+  {
+    "revision": "7e39c7bcc0d5bfd6798e931fe2925232",
+    "url": "/comida-para-todos/static/media/Voluntario1.7e39c7bc.jpg"
+  },
+  {
+    "revision": "3041448e741b2e7b3d821132dc9ea80e",
+    "url": "/comida-para-todos/static/media/Voluntario3.3041448e.png"
+  },
+  {
+    "revision": "7d4acba7e8457b1f06b566495b67195a",
+    "url": "/comida-para-todos/static/media/apadrinar.7d4acba7.png"
+  },
+  {
+    "revision": "a1b2f6bd2426c8434ff1844f78a1905e",
+    "url": "/comida-para-todos/static/media/arrow-left.a1b2f6bd.png"
+  },
+  {
+    "revision": "70854293c2de45d1e0e0f0cb2a1c4214",
+    "url": "/comida-para-todos/static/media/arrow-right.70854293.png"
+  },
+  {
+    "revision": "0008a3f8cc905c97f8e04cf394f3fa3b",
+    "url": "/comida-para-todos/static/media/banco-alimentos.0008a3f8.jpg"
+  },
+  {
+    "revision": "9516e3810f69e540efdf1e87899f5d2a",
+    "url": "/comida-para-todos/static/media/carbon_email.9516e381.png"
+  },
+  {
+    "revision": "a6c8ca6259a08eac172cab68106d0ec8",
+    "url": "/comida-para-todos/static/media/casa.a6c8ca62.png"
+  },
+  {
+    "revision": "226172c2d84a8cc1b3795080185004f0",
+    "url": "/comida-para-todos/static/media/charts.226172c2.png"
+  },
+  {
+    "revision": "3877e6f94a0759978085be085ea8fab8",
+    "url": "/comida-para-todos/static/media/cil_plant.3877e6f9.png"
+  },
+  {
+    "revision": "c533992544a41485bcac7a53c3a43bc4",
+    "url": "/comida-para-todos/static/media/clarity_phone-handset-line.c5339925.png"
+  },
+  {
+    "revision": "c473a0ce7aa133cdc4390bca868c22f2",
+    "url": "/comida-para-todos/static/media/clase.c473a0ce.png"
+  },
+  {
+    "revision": "c6034fe4c1980acd9e7090de48b13960",
+    "url": "/comida-para-todos/static/media/claudio.c6034fe4.png"
+  },
+  {
+    "revision": "d79e8b0285064fc31e5cb6e10b83f4f1",
+    "url": "/comida-para-todos/static/media/comvul.d79e8b02.png"
+  },
+  {
+    "revision": "203eb4285cd9a15d98469e7c52d347a5",
+    "url": "/comida-para-todos/static/media/descargar.203eb428.png"
+  },
+  {
+    "revision": "1725cac6a548f0ba14ec5c8e1366dd4a",
+    "url": "/comida-para-todos/static/media/desperdicios.1725cac6.jpg"
+  },
+  {
+    "revision": "8bc0f4fcd1a1a0c31350235fb6ff44ff",
+    "url": "/comida-para-todos/static/media/dona-alimentos.8bc0f4fc.png"
+  },
+  {
+    "revision": "d6dabf341abc32860ce02b7e093375a3",
+    "url": "/comida-para-todos/static/media/dona-clase.d6dabf34.png"
+  },
+  {
+    "revision": "995888506b4a0ee139f13b94c0af22b1",
+    "url": "/comida-para-todos/static/media/dona-habilidad.99588850.png"
+  },
+  {
+    "revision": "6abe13eddd4ccf2ff4efd9aeb3260468",
+    "url": "/comida-para-todos/static/media/dona-huertos.6abe13ed.png"
+  },
+  {
+    "revision": "2ed33cc14bd532c3a4759aaf13701d22",
+    "url": "/comida-para-todos/static/media/dona.2ed33cc1.png"
+  },
+  {
+    "revision": "d4790eb1587f47f47575b4244061d605",
+    "url": "/comida-para-todos/static/media/donaclase.d4790eb1.png"
+  },
+  {
+    "revision": "9e4547f1d61a975ccd9c3ae7c8e1c78c",
+    "url": "/comida-para-todos/static/media/edit-2.9e4547f1.png"
+  },
+  {
+    "revision": "a883085e842f238d29aa21344fa041f6",
+    "url": "/comida-para-todos/static/media/envios.a883085e.png"
+  },
+  {
+    "revision": "db210e45fecd566edff25e7719844a62",
+    "url": "/comida-para-todos/static/media/equipo-3.db210e45.jpg"
+  },
+  {
+    "revision": "b2d5f8335f596a71d1cf0f5d48db0156",
+    "url": "/comida-para-todos/static/media/familia.b2d5f833.png"
+  },
+  {
+    "revision": "76e7dbaf1b1a0bd6dfc606332e2de0a4",
+    "url": "/comida-para-todos/static/media/furgon-escolar.76e7dbaf.png"
+  },
+  {
+    "revision": "494554a072f6947b77e5004e10983fc9",
+    "url": "/comida-para-todos/static/media/habilidad.494554a0.png"
+  },
+  {
+    "revision": "afd038d8aeb00ef9709f11c6d213af05",
+    "url": "/comida-para-todos/static/media/hazte-voluntario.afd038d8.png"
+  },
+  {
+    "revision": "046b50081583d7e6c7b9035040045f4d",
+    "url": "/comida-para-todos/static/media/img2.046b5008.jpg"
+  },
+  {
+    "revision": "7ec312f36307731053db4cf4eb3cc409",
+    "url": "/comida-para-todos/static/media/img3.7ec312f3.jpg"
+  },
+  {
+    "revision": "82462b844b3288a72625aa4a2a8ce648",
+    "url": "/comida-para-todos/static/media/impact.82462b84.jpg"
+  },
+  {
+    "revision": "f1626fd07f160e0423b61d386985e20a",
+    "url": "/comida-para-todos/static/media/internet.f1626fd0.png"
+  },
+  {
+    "revision": "2e2c99e236ede16ecd0939796576422c",
+    "url": "/comida-para-todos/static/media/logo-ods-blanco.2e2c99e2.png"
+  },
+  {
+    "revision": "ab49558e79a807733d4f21236bd713cd",
+    "url": "/comida-para-todos/static/media/logo.ab49558e.png"
+  },
+  {
+    "revision": "e73a5283d5fc33cda8c43745189275fc",
+    "url": "/comida-para-todos/static/media/logo2.e73a5283.png"
+  },
+  {
+    "revision": "45803edf6f85762be209c065a99300ad",
+    "url": "/comida-para-todos/static/media/mach.45803edf.png"
+  },
+  {
+    "revision": "16f355f55a3359b9363f836f496c5c99",
+    "url": "/comida-para-todos/static/media/map-pin.16f355f5.png"
+  },
+  {
+    "revision": "810bd80ce33a5a2609a6f35b8308819b",
+    "url": "/comida-para-todos/static/media/modal.810bd80c.jpg"
+  },
+  {
+    "revision": "e111e55df46e954d2208340e51c241f7",
+    "url": "/comida-para-todos/static/media/olla.e111e55d.png"
+  },
+  {
+    "revision": "5d1ff19362b3097312a889590172df3a",
+    "url": "/comida-para-todos/static/media/plato.5d1ff193.gif"
+  },
+  {
+    "revision": "0d95fee6a376b3d65cd867da595cad53",
+    "url": "/comida-para-todos/static/media/porcentaje1.0d95fee6.png"
+  },
+  {
+    "revision": "d9d6368c0be35227ee1c88bdc4fd0d16",
+    "url": "/comida-para-todos/static/media/porcentaje2.d9d6368c.png"
+  },
+  {
+    "revision": "01ecd1962642fc7277b395519b763e66",
+    "url": "/comida-para-todos/static/media/portada.01ecd196.jpg"
+  },
+  {
+    "revision": "34ceac10c0272dc76e35f158857136eb",
+    "url": "/comida-para-todos/static/media/portada.34ceac10.png"
+  },
+  {
+    "revision": "fe1aa35db2c4e7341a861a95103419c3",
+    "url": "/comida-para-todos/static/media/productores.fe1aa35d.png"
+  },
+  {
+    "revision": "3b4b407ad679f07999ebc7b913ae35ef",
+    "url": "/comida-para-todos/static/media/restaurantes.3b4b407a.png"
+  },
+  {
+    "revision": "92bf7dcbed2d604fe4729d08d0b210bc",
+    "url": "/comida-para-todos/static/media/restaurants.92bf7dcb.png"
+  },
+  {
+    "revision": "3422c23feb7971ea9492dd111ba3baa6",
+    "url": "/comida-para-todos/static/media/sectores.3422c23f.png"
+  },
+  {
+    "revision": "520fce1ed6d2be77b3d894813d422338",
+    "url": "/comida-para-todos/static/media/sustentable_13.520fce1e.png"
+  },
+  {
+    "revision": "f98cc034b507e6758cc7c5ebc2d60fd8",
+    "url": "/comida-para-todos/static/media/sustentable_17.f98cc034.png"
+  },
+  {
+    "revision": "98c90e8b477f6270afb8984dff551392",
+    "url": "/comida-para-todos/static/media/take.98c90e8b.png"
+  },
+  {
+    "revision": "5490cc94688597e78a3d80d5bee0cd5a",
+    "url": "/comida-para-todos/static/media/tarjeta-capacitación.5490cc94.png"
+  },
+  {
+    "revision": "b54a6da2b22e32d7f8d04cf9e3ee5022",
+    "url": "/comida-para-todos/static/media/tarjeta-comunidad.b54a6da2.png"
+  },
+  {
+    "revision": "308d63b9dedcea216f4404671b6c631d",
+    "url": "/comida-para-todos/static/media/tarjeta-restaurant.308d63b9.png"
+  },
+  {
+    "revision": "ca7cf24a8770c42c2c24122bb08774c9",
+    "url": "/comida-para-todos/static/media/transparencia.ca7cf24a.png"
+  },
+  {
+    "revision": "599667cf439194e4111627bbaef28eb2",
+    "url": "/comida-para-todos/static/media/transporte.599667cf.png"
+  },
+  {
+    "revision": "86eb15591aa3fcad489a90ccb83bef75",
+    "url": "/comida-para-todos/static/media/transporte.86eb1559.png"
+  },
+  {
+    "revision": "a0b839b0d547cff03a106db73480989f",
+    "url": "/comida-para-todos/static/media/unnamed 1.a0b839b0.png"
+  },
+  {
+    "revision": "d7a224ded7406007a7b4b45da7cc90b0",
+    "url": "/comida-para-todos/static/media/user-outlined.d7a224de.png"
+  },
+  {
+    "revision": "280cffd5d352ca9950ae763c49d74bbf",
+    "url": "/comida-para-todos/static/media/video.280cffd5.mp4"
+  },
+  {
+    "revision": "6c47fa28b65aec40d83ff7e2183e5fd7",
+    "url": "/comida-para-todos/static/media/voluntario2.6c47fa28.jpg"
+  }
+]);
